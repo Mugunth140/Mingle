fb5651ef4efcbd57912b536dfee0a9bd4dd6669c
mugunth140 <mugunth140@gmail.com>
2024-12-29 14:52:00 +0530

    vercel client production test 1.0

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import  axios from 'axios';
 
 export const Server = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api" : "/api",
+    baseURL: import.meta.env.CLIENT_MODE === "production" ? `${import.meta.env.SERVER_URL}/api` : "http://localhost:8080/api",
     withCredentials: true,
 })
 
